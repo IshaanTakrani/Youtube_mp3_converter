4d818aa1185aa26be0f6d886c2449fec42ba1bbd
IshaanTakrani <ishaantakrani@gmail.com>
2024-02-25 12:40:46 -0500

    added executable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("-----------------------------------------------------------------------")
+print("-"*71)
 from pytube import YouTube 
 import os
 import music_tag
@@ -6,55 +6,62 @@ from moviepy.editor import VideoFileClip
 from pathlib import Path
 
 print('''
+Ishaan Takrani's
               _____ 
 /\_/\___  _   _| |_ _   _| |__   ___  | |_ ___    _ __ ___  _ __|___ / 
 \_ _/ _ \| | | | __| | | | '_ \ / _ \ | __/ _ \  | '_ ` _ \| '_ \ |_ \ 
  / \ (_) | |_| | |_| |_| | |_) |  __/ | || (_) | | | | | | | |_) |__) |
  \_/\___/ \__,_|\__|\__,_|_.__/ \___|  \__\___/  |_| |_| |_| .__/____/ 
                                                            |_|         
-v 1.1
+v 1.2
       ''')
 
-yt = YouTube(str(input("Enter URL: \n>> "))) 
+continue_status = True
 
-print("Working...")
+while(continue_status):
 
-if(" - ") in yt.title:
-    yt.title = yt.title.split(" - ")[1]
-  
-video = yt.streams.filter().first()
+    yt = YouTube(str(input("Enter URL: \n>> "))) 
 
-inp_path = str(input("Enter Directory (curr for current directory, empty for default music directory): \n>> "))
+    print("Working...")
 
-if (inp_path == "curr"):
-    destination = str('.')
-elif (inp_path == ''):
-    destination = str(Path.home() / "Music")
-else:
-    destination = str(rf'{inp_path}')
+    if(" - ") in yt.title:
+        yt.title = yt.title.split(" - ")[1]
+    
+    video = yt.streams.filter().first()
 
-out_file = video.download(output_path=destination) 
+    inp_path = str(input("Enter Directory (curr for current directory, empty for default music directory): \n>> "))
 
-base, _ = os.path.splitext(out_file)
-new_file = base + '.mp4'
-os.rename(out_file, new_file)
+    if (inp_path == "curr"):
+        destination = str('.')
+    elif (inp_path == ''):
+        destination = str(Path.home() / "Music")
+    else:
+        destination = str(rf'{inp_path}')
 
-video = VideoFileClip(base + ".mp4")
-video.audio.write_audiofile(base + ".mp3")
-video.close()
+    out_file = video.download(output_path=destination) 
 
-file_path = new_file
-f = music_tag.load_file(base + ".mp3")
-f["title"] = yt.title
-f['artist'] = yt.author
+    base, _ = os.path.splitext(out_file)
+    new_file = base + '.mp4'
+    os.rename(out_file, new_file)
 
+    video = VideoFileClip(base + ".mp4")
+    video.audio.write_audiofile(base + ".mp3")
+    video.close()
 
-f.save()
-print("Success! Video downloaded as mp3")
+    file_path = new_file
+    f = music_tag.load_file(base + ".mp3")
+    f["title"] = yt.title
+    f['artist'] = yt.author
 
+    f.save()
+    print("Success! Video downloaded as mp3")
 
-try:
     os.remove(base + ".mp4")
-except OSError as e:
-    print(f"Error deleting original MP4 file: {e}")
+    
+    continue_status = str(input("Convert another file(Y/n)\n>> "))
+
+    if(continue_status.lower() != "y" or continue_status.lower() != "yes"):
+        break
+
+print("-"*71)
 
